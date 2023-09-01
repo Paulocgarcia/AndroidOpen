309808f95b803fa358f36761e78fb91e72a57eb1
NOTEBOOKHP-G42 <paulocgarcia.garcia@gmail.com>
2023-08-31 21:07:17 -0300

    alteração Teste 1

--- app/src/main/java/com/example/androidopen/MainActivity.kt
+++ app/src/main/java/com/example/androidopen/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidopen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//Teste 1
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
