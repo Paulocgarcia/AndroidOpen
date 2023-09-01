80e0151fb94caa783d4313be01a40959774c064b
NOTEBOOKHP-G42 <paulocgarcia.garcia@gmail.com>
2023-08-31 21:45:58 -0300

    comentario de alteração 1.1

--- app/src/main/java/com/example/androidopen/MainActivity.kt
+++ app/src/main/java/com/example/androidopen/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.androidopen
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//comentario de alteração 1.1
+
 //Teste 1
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
